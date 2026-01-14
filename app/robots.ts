a49b03bf078db0d7669c2be5b57a80b4c26dba9d
koza501-cell <koza501@gmail.com>
2026-01-15 07:42:01 +0900

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,18 @@ export default function robots(): MetadataRoute.Robots {
         userAgent: '*',
         allow: '/',
         disallow: [
+          '/wp-content/',
+          '/wp-admin/',
+          '/wp-includes/',
+          '/wp-json/',
+          '/?p=',
+          '/product-category/',
+          '/shop/',
+          '/xmlrpc.php',
+          '/wp-login.php',
+          '/feed/',
+          '/trackback/',
+          '/comments/',
           '/api/',
           '/admin/',
           '/checkout',
@@ -15,6 +27,6 @@ export default function robots(): MetadataRoute.Robots {
         ],
       },
     ],
-    sitemap: 'https://newjapandeals.com/sitemap.xml',
+    sitemap: 'https://www.newjapandeals.com/sitemap.xml',
   };
 }
